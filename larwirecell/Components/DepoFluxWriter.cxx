6400b143780cc57b86453c4b50a054db6254a000
lynnt20 <lynnt@uchicago.edu>
2025-02-18 17:13:03 -0600

    check that cfg member is empty, not that it exists

--- larwirecell/Components/DepoFluxWriter.cxx
+++ larwirecell/Components/DepoFluxWriter.cxx
@@ -128,8 +128,7 @@ void DepoFluxWriter::configure(const WireCell::Configuration& cfg)
   }
 
   m_process_planes = {0, 1, 2};
-
-  if (cfg.isMember("process_planes")) {
+  if (!cfg["process_planes"].empty()) {
     m_process_planes.clear();
     for (auto jplane : cfg["process_planes"]) {
       m_process_planes.push_back(jplane.asInt());
